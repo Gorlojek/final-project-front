380c35f03b649a6d8a1f71de51983d8b61ad7246
Gorlojek <maciej.bus1@gmail.com>
2021-10-15 19:02:57 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Club } from './club';
+
+import { ClubService } from './club.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'final-project-app';
+export class AppComponent implements OnInit{
+  public clubs: Club[] = []; 
+
+  constructor(private clubService: ClubService) { }
+
+  ngOnInit(){
+    this.getClubs();
+  }
+
+  public getClubs(): void {
+    this.clubService.getClubs().subscribe(
+      (response: Club[]) => {
+        this.clubs = response;
+      },
+      (error: HttpErrorResponse)=>{
+        alert(error.message);
+      }
+    )
+  }
 }
---
+++ src/app/club.ts
@@ -0,0 +1,11 @@
+export interface Club{
+    id: number;
+    points: number;
+    name: string;
+    yearOfFoundation: number;
+    city: string,
+    countryCode: string;
+    position: number;
+    leagueCode: string;
+    active: boolean;   
+} 
